ca797b6c91ccd8eaa0d6dfe2d1a9408220de907c
MarcKyle <jimena.marckyle123@gmail.com>
2025-02-21 01:14:37 +0800

    Changes in ScreenPage2

--- UCGator/src/screens/onboarding_screens/ScreenPage2.tsx
+++ UCGator/src/screens/onboarding_screens/ScreenPage2.tsx
@@ -1,7 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
-
-
 import { ImageBackground, Image, Text, View } from 'react-native'
 
 import styles from './Onboarding.Style'
@@ -9,66 +6,15 @@ import styles from './Onboarding.Style'
 const ScreenPage2 = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.textContainer}>
-                <Text style={styles.title}>Manage Your Time Better</Text>
-                <Text style = {styles.description}>
-                    With UCGator, our guidance
-                    allows you to manage your time better.
-                </Text>
-            </View>
-            <View style = {styles.imageContainer}>
-                <Image 
-                    source={require("../../assets/onboarding_assets/splash_2_person.png")} 
-                    style={styles.image}
-                />
-            </View>  
+            <ImageBackground source={require('../../assets/onboarding_assets/bgraph_2.png')} resizeMode="stretch" style={styles.background}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.textTitle}>Manage Your {'\n'}Time Better </Text>
+                    <Text style={styles.textSubtitle}>With UCGator, our guidance allows you to manage your time better.</Text>
+                </View>
+                <Image source={require('../../assets/onboarding_assets/img_lght_2.png')} style={styles.imageHighlight} />
+            </ImageBackground>
         </View>
     )
 };
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "flex-start",
-        justifyContent: "space-between",
-        paddingTop: 80,
-        padding: 40,
-    },
-
-    gradientImage: {
-
-    },
-
-    // Text
-    textContainer: {
-        alignItems: "flex-start",
-        width: "100%",
-    },
-    title: {
-        fontSize: 45,
-        fontWeight: "bold",
-        textAlign: "left",
-        color: "#000",
-    },
-    description: {
-        fontSize: 16,
-        textAlign: "left",
-        color: "#666",
-        marginTop: 8,
-    },
-
-     // Image
-     imageContainer: {
-        alignItems: "flex-start",
-        marginBottom: 20,
-    },
-    image: {
-        width: 500,
-        height: 500,
-        resizeMode: "contain",
-        alignSelf: "flex-end",
-    },
-});
 
 export default ScreenPage2
